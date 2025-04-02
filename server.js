49624ad0a80e90c64d74af732e830aabb0ccfb6e
Nikola Novak <nikola.novak.hr@gmail.com>
2025-04-02 21:47:53 +0200

    feat: Implement basic HTTP server with file serving capabilities and error handling

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 import http from 'http'
+import fs from 'fs/promises'
+import url from 'url'
+import path from 'path'
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html')
-    // res.statusCode = 200
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('<h1>Hello World</h1>')
+const server = http.createServer(async (req, res) => {
+    try {
+        if (req.method === 'GET') {
+            let filePath
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html')
+            } else if (req.url === "/about") {
+                filePath = path.join(__dirname, 'public', 'about.html')
+            } else {
+                filePath = path.join(__dirname, 'public', '404.html')
+            }
+            const data = await fs.readFile(filePath, 'utf-8')
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data)
+        } else {
+            throw new Error('Method not allowed')
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' })
+        res.end('500 Internal Server Error')
+    }
+
 })
 
 server.listen(PORT, () => {
