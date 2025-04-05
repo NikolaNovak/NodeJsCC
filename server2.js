64ee61f034679eaa1973a42c64574e09559158ec
Nikola Novak <nikola.novak.hr@gmail.com>
2025-04-05 23:37:45 +0200

    feat: refactor server2.js to modularize request handlers and add JSON middleware

--- server2.js
+++ server2.js
@@ -13,31 +13,51 @@ const loggerMiddleware = (req, res, next) => {
 	next()
 }
 
+const jsonMiddleware = (req, res, next) => {
+	res.setHeader("Content-Type", "application/json")
+	next()
+}
+
+const getUsersHandler = (req, res) => {
+	res.statusCode = 200
+	res.write(JSON.stringify(users))
+	res.end()
+}
+
+const getUserByIdHandler = (req, res) => {
+	const id = req.url.split("/")[3]
+	const user = users.find((user) => user.id === Number.parseInt(id))
+
+	if (user) {
+		res.statusCode = 200
+		res.write(JSON.stringify(user))
+	} else {
+		res.statusCode = 404
+		res.write(JSON.stringify({ message: "User not found" }))
+	}
+	res.end()
+}
+
+const notFoundHandler = (req, res) => {
+	res.statusCode = 404
+	res.write(JSON.stringify({ message: "Route not found" }))
+	res.end()
+}
+
 const server = createServer((req, res) => {
 	loggerMiddleware(req, res, () => {
-		if (req.url === "/api/users" && req.method === "GET") {
-			res.writeHead(200, { "Content-Type": "application/json" })
-			res.end(JSON.stringify(users))
-		} else if (
-			req.url.match(/\/api\/users\/([0-9]+)/) &&
-			req.method === "GET"
-		) {
-			const id = req.url.split("/")[3]
-			const user = users.find((user) => user.id === Number.parseInt(id))
-
-			res.setHeader("Content-Type", "application/json")
-			if (user) {
-				res.statusCode = 200
-				res.write(JSON.stringify(user))
+		jsonMiddleware(req, res, () => {
+			if (req.url === "/api/users" && req.method === "GET") {
+				getUsersHandler(req, res)
+			} else if (
+				req.url.match(/\/api\/users\/([0-9]+)/) &&
+				req.method === "GET"
+			) {
+				getUserByIdHandler(req, res)
 			} else {
-				res.statusCode = 404
-				res.write(JSON.stringify({ message: "User not found" }))
+				notFoundHandler(req, res)
 			}
-			res.end()
-		} else {
-			res.writeHead(404, { "Content-Type": "application/json" })
-			res.end(JSON.stringify({ message: "Route not found" }))
-		}
+		})
 	})
 })
 
