6f30888f34c4d3c4d916f8b8f1da8e44932b3075
Nikola Novak <nikola.novak.hr@gmail.com>
2025-04-05 23:33:21 +0200

    feat: add logger middleware

--- server2.js
+++ server2.js
@@ -8,27 +8,37 @@ const users = [
 	{ id: 3, name: "Jim Doe" },
 ]
 
-const server = createServer((req, res) => {
-	if (req.url === "/api/users" && req.method === "GET") {
-		res.writeHead(200, { "Content-Type": "application/json" })
-		res.end(JSON.stringify(users))
-	} else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-		const id = req.url.split("/")[3]
-		const user = users.find((user) => user.id === Number.parseInt(id))
+const loggerMiddleware = (req, res, next) => {
+	console.log(`[logger] ${req.method} ${req.url}`)
+	next()
+}
 
-		res.setHeader("Content-Type", "application/json")
-		if (user) {
-			res.statusCode = 200
-			res.write(JSON.stringify(user))
+const server = createServer((req, res) => {
+	loggerMiddleware(req, res, () => {
+		if (req.url === "/api/users" && req.method === "GET") {
+			res.writeHead(200, { "Content-Type": "application/json" })
+			res.end(JSON.stringify(users))
+		} else if (
+			req.url.match(/\/api\/users\/([0-9]+)/) &&
+			req.method === "GET"
+		) {
+			const id = req.url.split("/")[3]
+			const user = users.find((user) => user.id === Number.parseInt(id))
+
+			res.setHeader("Content-Type", "application/json")
+			if (user) {
+				res.statusCode = 200
+				res.write(JSON.stringify(user))
+			} else {
+				res.statusCode = 404
+				res.write(JSON.stringify({ message: "User not found" }))
+			}
+			res.end()
 		} else {
-			res.statusCode = 404
-			res.write(JSON.stringify({ message: "User not found" }))
+			res.writeHead(404, { "Content-Type": "application/json" })
+			res.end(JSON.stringify({ message: "Route not found" }))
 		}
-		res.end()
-	} else {
-		res.writeHead(404, { "Content-Type": "application/json" })
-		res.end(JSON.stringify({ message: "Route not found" }))
-	}
+	})
 })
 
 server.listen(PORT, () => {
