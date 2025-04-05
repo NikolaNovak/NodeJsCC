7bbf2fc393ea61a9ead9abd60c9eaa2d3cc916f0
Nikola Novak <nikola.novak.hr@gmail.com>
2025-04-05 23:29:41 +0200

    chore: add server2 and add @biomejs/biome dependency

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-import http from 'http'
-import fs from 'fs/promises'
-import url from 'url'
-import path from 'path'
+import http from "node:http"
+import fs from "node:fs/promises"
+import url from "node:url"
+import path from "node:path"
 
 const PORT = process.env.PORT
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const server = http.createServer(async (req, res) => {
-    try {
-        if (req.method === 'GET') {
-            let filePath
-            if (req.url === '/') {
-                filePath = path.join(__dirname, 'public', 'index.html')
-            } else if (req.url === "/about") {
-                filePath = path.join(__dirname, 'public', 'about.html')
-            } else {
-                filePath = path.join(__dirname, 'public', '404.html')
-            }
-            const data = await fs.readFile(filePath, 'utf-8')
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end(data)
-        } else {
-            throw new Error('Method not allowed')
-        }
-    } catch (error) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' })
-        res.end('500 Internal Server Error')
-    }
-
+	try {
+		if (req.method === "GET") {
+			let filePath
+			if (req.url === "/") {
+				filePath = path.join(__dirname, "public", "index.html")
+			} else if (req.url === "/about") {
+				filePath = path.join(__dirname, "public", "about.html")
+			} else {
+				filePath = path.join(__dirname, "public", "404.html")
+			}
+			const data = await fs.readFile(filePath, "utf-8")
+			res.writeHead(200, { "Content-Type": "text/html" })
+			res.end(data)
+		} else {
+			throw new Error("Method not allowed")
+		}
+	} catch (error) {
+		res.writeHead(500, { "Content-Type": "text/plain" })
+		res.end("500 Internal Server Error")
+	}
 })
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+	console.log(`Server is running on port ${PORT}`)
 })
---
+++ server2.js
@@ -0,0 +1,36 @@
+import { createServer } from "node:http"
+
+const PORT = process.env.PORT
+
+const users = [
+	{ id: 1, name: "John Doe" },
+	{ id: 2, name: "Jane Doe" },
+	{ id: 3, name: "Jim Doe" },
+]
+
+const server = createServer((req, res) => {
+	if (req.url === "/api/users" && req.method === "GET") {
+		res.writeHead(200, { "Content-Type": "application/json" })
+		res.end(JSON.stringify(users))
+	} else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+		const id = req.url.split("/")[3]
+		const user = users.find((user) => user.id === Number.parseInt(id))
+
+		res.setHeader("Content-Type", "application/json")
+		if (user) {
+			res.statusCode = 200
+			res.write(JSON.stringify(user))
+		} else {
+			res.statusCode = 404
+			res.write(JSON.stringify({ message: "User not found" }))
+		}
+		res.end()
+	} else {
+		res.writeHead(404, { "Content-Type": "application/json" })
+		res.end(JSON.stringify({ message: "Route not found" }))
+	}
+})
+
+server.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
