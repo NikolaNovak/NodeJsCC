e991579de2de7963af4fead1ab02f6ae4c38478c
Nikola Novak <nikola.novak.hr@gmail.com>
2025-05-26 23:35:25 +0200

    feat: init express.js and add some boilerplate

---
+++ express.js
@@ -0,0 +1,19 @@
+import express from "express"
+import path from "node:path"
+import url from "node:url"
+
+const app = express()
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+app.get("/about", (req, res) => {
+	res.sendFile(path.join(__dirname, "public", "about.html"))
+})
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000")
+})
